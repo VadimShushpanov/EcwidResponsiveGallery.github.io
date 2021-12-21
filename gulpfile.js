b62ac8e84abf56ca8dc02f488604b4be7ae00c48
vadim <vadim@daotech.ru>
2021-12-21 04:15:18 +0400

    added gulp with sass

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+let gulp = require('gulp');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass')(require('sass')) ;
+let browserSync = require('browser-sync').create();
+
+function scssToCss(done) {
+  
+  gulp.src('./scss/**/*.scss')
+  .pipe(sass({
+    errorLogToConsole: true,
+    outputStyle: 'compressed'
+  }))
+  .on('error', console.error.bind(console))
+  .pipe(rename({suffix: '.min'}))
+  .pipe( gulp.dest('./css/'))
+  .pipe( browserSync.stream());
+
+  done();
+}
+
+function sync(done){
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    port:3000
+  })
+  done();
+}
+
+function browserReload(done){
+  browserSync.reload();
+  done();
+}
+
+function watchFiles() {
+  gulp.watch("./scss/**/*", scssToCss);
+  gulp.watch("./**/*.html", browserReload);
+  gulp.watch("./**/*.js", browserReload);
+}
+
+gulp.task('default', gulp.parallel(sync, watchFiles));
